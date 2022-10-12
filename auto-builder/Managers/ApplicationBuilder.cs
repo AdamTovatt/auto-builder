ec38400784444e1952053207f6befc5b3e4c7f63
Adam Tovatt <sporedenstore@hotmail.com>
2022-10-12 14:55:50 +0200

    init

---
+++ auto-builder/Managers/ApplicationBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace auto_builder.Managers
+{
+    public class ApplicationBuilder
+    {
+        public static string ConfigurationFileName { get; set; } = "auto-builder-configuration.json";
+
+        /// <summary>
+        /// A list of applications that the application builder will support
+        /// </summary>
+        [JsonProperty("applications")]
+        public List<string> Applications { get; set; } = new List<string>();
+
+        public static ApplicationBuilder FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<ApplicationBuilder>(json);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ auto-builder/Models/Application.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace auto_builder.Models
+{
+    public class Application
+    {
+        /// <summary>
+        /// A human readable name for the application
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name of the build script that should run when the application needs to be built
+        /// </summary>
+        [JsonProperty("buildScriptName")]
+        public string BuildScriptName { get; set; }
+
+        /// <summary>
+        /// The path to folder with the source code, used for git
+        /// </summary>
+        [JsonProperty("sourceFolderPath")]
+        public string SourceFolderPath { get; set; }
+
+        /// <summary>
+        /// The commit hash of the last commit that was built
+        /// </summary>
+        [JsonProperty("lastBuiltCommit")]
+        public string LastBuiltCommit { get; set; }
+
+        /// <summary>
+        /// The last time the application was built
+        /// </summary>
+        [JsonProperty("lastBuildTime")]
+        public DateTime LastBuildTime { get; set; }
+    }
+}
